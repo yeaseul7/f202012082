10ad5a4ea618f67c3d799bb55eaefc2ac31f2c57
seulseul_0123 <cutie32769@gmail.com>
2023-10-18 11:13:01 +0900

    two

--- src/f202012082/Main.java
+++ src/f202012082/Main.java
@@ -3,6 +3,7 @@ package f202012082;
 
 public class Main {
 	public static void main(String[] args) {
+		Two.print();
 		System.out.println("one 202012082");
 	}
 }
---
+++ src/f202012082/Two.java
@@ -0,0 +1,7 @@
+package f202012082;
+
+public class Two {
+	public static void print() {
+		System.out.println("two");
+	}
+}
