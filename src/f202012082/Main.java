0f3f95abb27c8fb27ae81102d43a4d0168c193f7
seulseul_0123 <cutie32769@gmail.com>
2023-10-18 11:21:04 +0900

    four

--- src/f202012082/Main.java
+++ src/f202012082/Main.java
@@ -3,7 +3,8 @@ package f202012082;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("one 202012082");
-		Three.print();
+		Two.print();
+		System.out.println("one 201514199");
+		Four.print();
 	}
 }
